3c5dea569e996fbfaeb8fe307fa916c679ae168b
krrybarc <krystian.rybarczyk@assecobs.pl>
2017-12-08 19:45:44 +0100

    Created a few tests in JUnit.

--- src/test/java/pl/sdacademy/animals/bear/BearSpec.groovy
+++ src/test/java/pl/sdacademy/animals/bear/BearSpec.groovy
@@ -1,34 +1,30 @@
 package pl.sdacademy.animals.bear
 
 import org.joda.time.DateTime
+import pl.sdacademy.animals.bear.time.TestClock
 import pl.sdacademy.animals.time.Clock
 import spock.lang.Specification
 
 class BearSpec extends Specification {
 
     def "Bear should be alive immediately after creation"() {
-        given:
-        int weight = 3
-        Bear bear = new BlackBear(weight)
-
         when:
-        boolean result = bear.isAlive()
+        def bear = new BlackBear(3)
 
         then:
-        result
+        bear.isAlive()
     }
 
     def "Bear should be alive if it has eaten within 10 days"() {
         given:
         int weight = 3
         Bear bear = new BlackBear(weight)
-        bear.eat()
 
         when:
-        boolean result = bear.isAlive()
+        bear.eat()
 
         then:
-        result
+        bear.isAlive()
     }
 
     def "Bear should not be alive if it has eaten within more than 10 days"() {
@@ -37,26 +33,23 @@ class BearSpec extends Specification {
         def clock = new TestClock()
         Bear bear = new BlackBear(weight, clock)
         bear.eat()
-        clock.changeTimeByDays(10)
 
         when:
-        boolean result = bear.isAlive()
+        clock.changeTimeByDays(10)
 
         then:
-        result == false
+        bear.isAlive() == false
     }
 
-    class TestClock implements Clock {
+    def "Bear uses the clock to update its last meal time during eating"() {
+        given:
+        Clock clock = Mock(Clock)
+        def bear = new BlackBear(1, clock)
 
-        private DateTime time = DateTime.now()
+        when:
+        bear.eat()
 
-        @Override
-        DateTime getCurrentTime() {
-            return time
-        }
-
-        public void changeTimeByDays(int days) {
-            time = time.plusDays(days)
-        }
+        then:
+        1 * clock.currentTime
     }
 }
---
+++ src/test/java/pl/sdacademy/animals/bear/BearTest.java
@@ -0,0 +1,33 @@
+package pl.sdacademy.animals.bear;
+
+import org.junit.jupiter.api.Test;
+import pl.sdacademy.animals.bear.time.TestClock;
+import pl.sdacademy.animals.time.Clock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BearTest {
+
+    @Test
+    public void bearShouldBeAliveImmediatelyAfterCreation() {
+        Bear bear = new BlackBear(3);
+        assertTrue(bear.isAlive());
+    }
+
+    @Test
+    public void bearShouldBeAliveIfItHasEatenWithin10Days() {
+        Bear bear = new BlackBear(3);
+        bear.eat();
+        assertTrue(bear.isAlive());
+    }
+
+    @Test
+    public void bearShouldNotBeAliveIfItHasEatenWithinMoreThan10Days() {
+        TestClock clock = new TestClock();
+        Bear bear = new BlackBear(3, clock);
+        bear.eat();
+        clock.changeTimeByDays(10);
+        assertFalse(bear.isAlive());
+    }
+
+}
